76b74041f237aaa98346eb7da8d4cf49e7b59bd6
Tresor-Kasenda <scotttresor@gmail.com>
2023-06-17 17:48:52 +0200

    initial project

---
+++ src/LaravelSkeletonServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Scott\LaravelSkeleton;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class LaravelSkeletonServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        // $package
+    }
+}
